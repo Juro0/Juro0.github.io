447a88ffe9c1b8592e60b611a651a6069cf8f449
Juri <78843415+Juro0@users.noreply.github.com>
2023-02-26 20:53:53 +0100

    v1.1.1: portfolio generator

---
+++ assets/js/portfolio.js
@@ -0,0 +1,37 @@
+let portfolio = [
+    {
+        "displayName": "NOB",
+        "img": "nob",
+        "desc": "Questo sito è stato realizzato per un bar 'non ordinario' locale. ho deciso di inserirlo nel portfolio perchè è il mio primo sito per attività realizzato.",
+        "link": "https://juro0.github.io/nob-site",
+        "responsive": true
+    },
+    {
+        "displayName": "BeReal. (doItWorst)",
+        "img": "bereal",
+        "desc": "Questo sito fa parte di un mio progetto, doItWorst, che consiste nel replicare e modificare siti internet già esistenti per allenarsi.",
+        "link": "https://juro0.github.io/doItWorst/BeReal",
+        "responsive": false
+    },
+    {
+        "displayName": "Exipom",
+        "img": "exipom",
+        "desc": "Exipom è un progetto opensource attualmente in fase di sviluppo basato sulla tecnica di concentrazione del pomodoro timer.",
+        "link": "https://juro0.github.io/exipom",
+        "responsive": true
+    },
+    {
+        "displayName": "Links",
+        "img": "links",
+        "desc": "Questo sito contiene vari links ai miei social network e altri miei account, ed è direttamente collegato al codice QR nel mio biglietto da visita.",
+        "link": "https://juro0.github.io/links",
+        "responsive": true
+    },
+    {
+        "displayName": "Simon game",
+        "img": "simon",
+        "desc": "Simon è una replica formato sito del vecchio gioco di SIMON, che consiste nel replicare una combinazione di mosse dettata dal computer tramite pulsanti colorati.",
+        "link": "https://juro0.github.io/simon-game",
+        "responsive": true
+    },
+]
